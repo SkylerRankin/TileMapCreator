b04543abe0fd48fc9d2e9f2e91501aabe158fb0b
Skyler <skylerrankin@gmail.com>
2018-02-03 13:27:55 -0500

    a

--- src/creator/Editor.java
+++ src/creator/Editor.java
@@ -27,7 +27,7 @@ public class Editor extends JPanel {
     				g.drawImage(tiles[gY][gX], (gX*gridSpacing), (gY*gridSpacing), null);
     				
     			}
-    		
+    		}
     		if (xDim == yDim) {
     			for (int i = 0; i < xDim; i+=gridSpacing) {
     				g.drawLine(i, 0, i, xDim);
